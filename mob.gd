90fa1abf96a8afcb3125ca5a9f2becf5356d3034
Hana Zait <hana.o.zait@gmail.com>
2025-10-19 18:48:05 -0700

    Add mob walk and hurt animations

--- mob.gd
+++ mob.gd
@@ -6,6 +6,11 @@ var health = 3
 @onready var player = get_node("/root/Game/Player")
 
 
+func _ready():
+	# Play slime walk animation
+	%Slime.play_walk()
+	
+
 func _physics_process(delta: float) -> void:
 	# Move mob towards player
 	var direction = global_position.direction_to(player.global_position)
@@ -15,7 +20,15 @@ func _physics_process(delta: float) -> void:
 	
 func take_damage():
 	health -= 1
+	# Play slim hurt animation
+	%Slime.play_hurt()
 	
 	# If mob has no health, it dies
 	if health == 0:
 		queue_free()
+		
+		const SMOKE_SCENE = preload("uid://dhmhmrth6rdce")
+		var smoke = SMOKE_SCENE.instantiate()
+		# Add smoke node as sibling of mob node, so that it doesn't disappear with mob
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position

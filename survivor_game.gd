58a0be0e238285b0579591adcd01e684eb60ce0f
Hana Zait <hana.o.zait@gmail.com>
2025-10-19 19:41:31 -0700

    Add mob random spawning

---
+++ survivor_game.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+func _ready() -> void:
+	spawn_mob()
+	spawn_mob()
+	spawn_mob()
+	spawn_mob()
+	spawn_mob()
+
+
+# Spawn mob at random location
+func spawn_mob():
+	#const MOB = preload("res://mob.tscn")
+	# Create new mob node
+	var new_mob = preload("res://mob.tscn").instantiate()
+	# Get random location for new mob
+	%PathFollow2D.progress_ratio = randf()
+	new_mob.global_position = %PathFollow2D.global_position
+	# Add mob node as child of Game node
+	add_child(new_mob)
+
+
+func _on_timer_timeout() -> void:
+	spawn_mob()
